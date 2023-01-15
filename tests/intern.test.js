da4e5885ef22161149a55767cef2cbddb601772f
RayR124 <RealtorRayR@Gmail.com>
2023-01-15 13:24:12 -0800

    added tests

---
+++ tests/intern.test.js
@@ -0,0 +1,9 @@
+const intern = require("../library/intern");
+
+test("Creates an object under Intern class", () => {
+    const intern = new Intern("Ray", 12345, "InternRay@FakeEmail.com", "UC Berkeley Extension Coding BootCamp");
+});
+
+test("Creates a role for Managerial duties", () => {
+    const intern = new Intern("Ray", 12345, "InternRay@FakeEmail.com", "UC Berkeley Extension Coding BootCamp");
+});
