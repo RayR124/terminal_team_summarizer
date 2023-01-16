2bb6c843c64d15dc435b4ab031d2972bea2fe2fc
RayR124 <RealtorRayR@Gmail.com>
2023-01-16 14:53:52 -0800

    deleted unnecessary tests and added require fs and jest

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -3,7 +3,3 @@ const engineer = require("../library/engineer");
 test("Creates an object under Engineer class", () => {
     const engineer = new Engineer("Ray", 12345, "EngineerRay@FakeEmail.com", "RayR124");
 });
-
-test("Creates a user value for their GitHub", () => {
-    const engineer = new Engineer("Ray", 12345, "EngineerRay@FakeEmail.com", "RayR124");
-});
